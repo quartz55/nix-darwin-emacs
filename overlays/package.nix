ae580118827f1f61ac11c8ac5ae14bd697e95cee
c4710n <c4710n@users.noreply.github.com>
2022-12-30 20:45:06 +0800

    feat: remove exwm which is useless on macOS

--- overlays/package.nix
+++ overlays/package.nix
@@ -1,30 +1,5 @@
 self: super:
-let
-  mkExDrv = emacsPackages: name: args:
-    let
-      repoMeta = super.lib.importJSON (../repos/exwm/. + "/${name}.json");
-    in
-    emacsPackages.melpaBuild (
-      args // {
-        pname = name;
-        ename = name;
-        version = repoMeta.version;
-        commit = repoMeta.rev;
 
-        recipe = builtins.toFile "recipe" ''
-          (${name} :fetcher github
-          :repo "ch11ng/${name}")
-        '';
-
-        src = super.fetchFromGitHub {
-          owner = "ch11ng";
-          repo = name;
-          inherit (repoMeta) rev sha256;
-        };
-      }
-    );
-
-in
 {
   emacsPackagesFor = emacs: (
     (super.emacsPackagesFor emacs).overrideScope' (
@@ -51,14 +26,6 @@ in
         // super.lib.optionalAttrs (super.lib.hasAttr "nongnuPackages" esuper) {
           nongnuPackages = esuper.nongnuPackages.override {
             generated = ../repos/nongnu/nongnu-generated.nix;
-          };
-        } // {
-          xelb = mkExDrv eself "xelb" {
-            packageRequires = [ eself.cl-generic eself.emacs ];
-          };
-
-          exwm = mkExDrv eself "exwm" {
-            packageRequires = [ eself.xelb ];
           };
         }
     )
