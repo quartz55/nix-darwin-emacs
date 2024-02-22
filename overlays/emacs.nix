cc8f1cb0de44ae2d5931ca1f68b01d4e2c4a2a6b
c4710n <c4710n@users.noreply.github.com>
2024-02-22 22:55:07 +0800

    refactor: use substituteInPlace --replace-warn

--- overlays/emacs.nix
+++ overlays/emacs.nix
@@ -25,8 +25,8 @@ let
 
             postPatch = old.postPatch + ''
               substituteInPlace lisp/loadup.el \
-              --replace '(emacs-repository-get-version)' '"${repoMeta.rev}"' \
-              --replace '(emacs-repository-get-branch)' '"${repoMeta.branch}"'
+              --replace-warn '(emacs-repository-get-version)' '"${repoMeta.rev}"' \
+              --replace-warn '(emacs-repository-get-branch)' '"${repoMeta.branch}"'
             '';
           }
         ))
@@ -54,7 +54,7 @@ let
                     ])));
                 in
                 ''
-                  substituteInPlace lisp/emacs-lisp/comp.el --replace \
+                  substituteInPlace lisp/emacs-lisp/comp.el --replace-warn \
                     "(defcustom comp-libgccjit-reproducer nil" \
                     "(setq native-comp-driver-options '(${backendPath}))
                      (defcustom comp-libgccjit-reproducer nil"
